0ab038540ef20935a979a92cfd604122e3935567
lucpattyn <ataul.mukit@gmail.com>
2019-07-02 21:10:11 +0600

    resolve conflict

--- src/quarks.cpp
+++ src/quarks.cpp
@@ -5,7 +5,6 @@ using namespace Quarks;
 
 Cache Cache::_Instance;
 Matrix Matrix::_Instance;
-<<<<<<< Updated upstream
 
 rocksdb::DB* db = nullptr;
 rocksdb::Status dbStatus;
@@ -20,9 +19,6 @@ void closeDB(){
     // close the database
     delete db;
 }
-
-=======
->>>>>>> Stashed changes
 
 int wildcmp(const char *wild, const char *string) {
     // Written by Jack Handy - <A href="mailto:jakkhandy@hotmail.com">jakkhandy@hotmail.com</A>
