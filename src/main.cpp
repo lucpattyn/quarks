18944ced8a6574e995deaceccbc79e9c543ca9b4
mukit <ataul.mukit@gmail.com>
2020-06-05 17:07:24 +0600

    clean up

--- src/main.cpp
+++ src/main.cpp
@@ -123,30 +123,7 @@ int main(int argc, char ** argv) {
 
    QSocket qsockFileUploader(CROW_ROUTE(app, "/ws/files/upload"), QSocket::FileInterceptor("upload"));
 
-	/*std::mutex mtx;;
-	CROW_ROUTE(app, "/ws/files/upload")
-        .websocket()
-        .onopen([&](crow::websocket::connection& conn){
-                CROW_LOG_INFO << "new websocket connection";
-                std::lock_guard<std::mutex> _(mtx);
-                //users.insert(&conn);
-                })
-        .onclose([&](crow::websocket::connection& conn, const std::string& reason){
-                CROW_LOG_INFO << "websocket connection closed: " << reason;
-                std::lock_guard<std::mutex> _(mtx);
-                //users.erase(&conn);
-                })
-        .onmessage([&](crow::websocket::connection& conn, const std::string& data, bool is_binary){
-                
-				 CROW_LOG_INFO << "binary data: " << is_binary;
-				//std::lock_guard<std::mutex> _(mtx);
-                //for(auto u:users)
-                //    if (is_binary)
-                //        u->send_binary(data);
-                //    else
-                //        u->send_text(data);
-                });*/
-				
+			
     
 ///////////////////////////////////////////
 /////////// core functionalities //////////
